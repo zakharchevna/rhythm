7eee228c1bf52b25fac673877505e582bb8df7e5
Olha Zakharchevna <zakharchevna@gmail.com>
2022-12-28 21:00:48 +0100

    add app-download section

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ const reviewsSwiper = document.querySelector('.swiper-reviews');
 
 if (reviewsSwiper) {
    const swiper = new Swiper('.swiper-reviews', {
+autoHeight: true,
 loop: true,
 pagination: {
    el: '.swiper-pagination',
+   clickable: true
 }
 });
 }
